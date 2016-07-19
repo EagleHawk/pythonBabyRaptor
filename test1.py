57319f26f7f3e5ff6fbb81492fa661c399839765
EagleHawk <eagle.soaringheights@gmail.com>
2016-07-19 20:14:39 +0530

    Basic Learning

---
+++ test1.py
@@ -0,0 +1,11 @@
+string1 = input("Enter name : ");
+print("hello ", string1);
+
+for letter in 'Python':     # First Example
+   print ('Current Letter :', letter)
+
+fruits = ['banana', 'apple',  'mango']
+for fruit in fruits:        # Second Example
+   print ('Current fruit :', fruit)
+
+print ("Good bye!")
