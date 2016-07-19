f2830e795a2a6c2c014d58d0a8ce8489dad4c058
EagleHawk <eagle.soaringheights@gmail.com>
2016-07-19 21:31:06 +0530

    Display Name
    Display the input name

--- test1.py
+++ test1.py
@@ -3,6 +3,7 @@ print("hello ", string1);
 
 for letter in 'Python':     # First Example
    print ('Current Letter :', letter)
+for letter in oString :
 
 fruits = ['banana', 'apple',  'mango']
 for fruit in fruits:        # Second Example
